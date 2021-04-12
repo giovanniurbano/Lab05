0aaa1ba922935d8e3ce1491cdab217a41bfefc2e
giovanniurbano <urbagio@gmail.com>
2021-04-12 19:41:20 +0200

    prima versione funzionante (controlli doppie lettere? controllo radice esistente? altri?)

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/FXMLController.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/FXMLController.java
@@ -1,6 +1,63 @@
 package it.polito.tdp.anagrammi;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtCorretti;
+
+    @FXML
+    private TextArea txtErrati;
+
+	private Model model;
+
+    @FXML
+    void doCalcolaAnagrammi(ActionEvent event) {
+    	String parola = txtParola.getText();
+    	List<List<String>> soluzione = new ArrayList<List<String>>(this.model.anagrammi(parola));
+    	txtCorretti.clear();
+    	txtErrati.clear();
+    	for(String a : soluzione.get(0))
+    		txtCorretti.appendText(a+"\n");
+    	for(String a : soluzione.get(1))
+    		txtErrati.appendText(a+"\n");
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtParola.clear();
+    	txtCorretti.clear();
+    	txtErrati.clear();
+    }
+
+    public void setModel(Model m) {
+    	this.model = m;
+    }
+    
+    @FXML
+    void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCorretti != null : "fx:id=\"txtCorretti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtErrati != null : "fx:id=\"txtErrati\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
---
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/db/AnagrammaDAO.java
@@ -0,0 +1,64 @@
+package it.polito.tdp.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AnagrammaDAO {
+
+	public boolean isCorrect(String anagramma) {
+		final String sql = "SELECT * FROM parola WHERE nome = ?";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, anagramma);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				rs.close();
+				st.close();
+				conn.close();
+				return true;
+			}
+			else {
+				rs.close();
+				st.close();
+				conn.close();
+				return false;
+			}
+		}
+		catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public boolean isRootCorrect(String anagramma) {
+		final String sql = "SELECT * FROM parola WHERE nome LIKE ?";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, anagramma+"%");
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				rs.close();
+				st.close();
+				conn.close();
+				return true;
+			}
+			else {
+				rs.close();
+				st.close();
+				conn.close();
+				return false;
+			}
+		}
+		catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
