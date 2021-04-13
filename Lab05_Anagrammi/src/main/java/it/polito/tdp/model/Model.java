9335be4fc0e3b08fb2f432ab404e21e83ade5032
giovanniurbano <urbagio@gmail.com>
2021-04-13 10:21:56 +0200

    controllo doppie

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/model/Model.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/model/Model.java
@@ -7,8 +7,6 @@ import it.polito.tdp.db.AnagrammaDAO;
 
 public class Model {
 	AnagrammaDAO dao;
-	private List<String> corretti;
-	private List<String> errati;
 	
 	public Model() {
 		dao = new AnagrammaDAO();
@@ -17,47 +15,33 @@ public class Model {
 	public List<List<String>> anagrammi(String parola) {
 		List<String> corretti = new ArrayList<String>();
 		List<String> errati = new ArrayList<String>();
-		permuta("", parola, 0, corretti, errati); // LANCIA la ricorsione
-		//rimuovo dalla lista parole che non esistono nel dizionario
+		
+		permuta("", parola, 0, corretti, errati); 
+		
 		List<List<String>> risultato = new ArrayList<>();
 		risultato.add(corretti);
 		risultato.add(errati);
+		
 		return risultato;
 	}
-	
-	// livello = lunghezza della soluzione parziale
-	// livello iniziale = 0
-	// parziale = stringa che contiene l'anagramma incompleto in fase di costruzione
-	// lettere = le lettere della parola iniziale che ancora non abbiamo utilizzato
-	//           (=== il sottoproblema che dobbiamo risolvere)
-	
+
 	private void permuta(String parziale, String lettere, int livello, List<String> corretti, List<String> errati) {
-		if(lettere.length() == 0) { // caso terminale 
-			// la soluzione parziale è anche una soluzione completa
-			//System.out.println(parziale);
-			//if(parziale è ancora parola valida)
-			if(dao.isCorrect(parziale))
+		if(lettere.length() == 0) {
+			if(dao.isCorrect(parziale) && !corretti.contains(parziale))
 				corretti.add(parziale);
-			else
+			if(!dao.isCorrect(parziale) && !errati.contains(parziale))
 				errati.add(parziale);
 		} 
 		else {
-			// fai ricorsione
-			// sottoproblema == una lettera (un singolo carattere) di 'lettere'
 			for(int pos=0; pos<lettere.length(); pos++) {
 				
 				char tentativo = lettere.charAt(pos); 
 				
 				String nuovaParziale = parziale + tentativo;
-				String nuovaLettere = lettere.substring(0, pos) + lettere.substring(pos+1); // toglie il carattere pos da lettere
+				String nuovaLettere = lettere.substring(0, pos) + lettere.substring(pos+1);
 				
-				//if(nuovaParziale è PREFISSO valido di almeno una parola nel dizionario)
 				if(dao.isRootCorrect(nuovaParziale))
 					permuta(nuovaParziale, nuovaLettere, livello+1, corretti, errati);
-				
-				// Backtracking (NON SERVE)
-				// rimetti a posto 'parziale'
-				// rimetti a posto 'lettere'
 				
 			}
 		}
